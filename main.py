7dbec1cbeefc05da1b9b6869cd0b36be7af52b1d
Rasmus Selsmark <rasmus@unity3d.com>
2022-11-24 23:37:32 +0100

    Use Flask webserver, show trains in simple table

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-import webapp3
-import websocket
-import json
+from flask import Flask, render_template
+import modules.dinstation
+
+app = Flask(__name__)
 
 
-class MainPage(webapp3.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+@app.route('/')
+def hello():
+    return 'pendler-statistik'
 
 
-class FetchData(webapp3.RequestHandler):
-    def get(self):
-        ws = websocket.WebSocket()
-        ws.connect('ws://labapiprod.dinstation.dk/api/ws/departure/HH/')
-        data = json.loads(ws.recv())
-        ws.close()
-
-        self.response.headers['Content-Type'] = 'text/plain'
-
-        for train in data['data']['Trains']:
-            self.response.write(f"#{train['TrainId']}, , Destination: {train['Routes'][0]['DestinationStationId']}, ScheduleTime: {train['ScheduleTime']}, IsCancelled: {train['IsCancelled']}, EstimatedTimeDeparture: {train['EstimatedTimeDeparture']}, DelayTime: {train['DelayTime']}, TrackCurrent: {train['TrackCurrent']}, TrackOriginal: {train['TrackOriginal']}\n")
+@app.route('/trains')
+def trains():
+    trains = modules.dinstation.get_trains("HH")
+    return render_template('trains.html', trains=trains)
 
 
-app = webapp3.WSGIApplication([
-    ('/', MainPage),
-    ('/fetch', FetchData),
-], debug=True)
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
